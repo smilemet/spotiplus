30d1d9520e3e671d83c64f9bf21d1396f99a0fcd
Everyruru <71490171+smilemet@users.noreply.github.com>
2022-09-25 01:15:10 +0900

    레이아웃 수정

--- src/Theme.js
+++ src/Theme.js
@@ -19,14 +19,28 @@ const theme = {
   // inner 안 max-width
   maxWidth: "670px",
 
+  // 영수증 폰트
+  receiptTitle: `
+    font-family: "Happiness-Sans-Bold";
+    src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2205@1.0/Happiness-Sans-Bold.woff2")
+      format("woff2");
+  `,
+
+  receiptFont: `
+    font-family: 'IBMPlexSansKR-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-07@1.0/IBMPlexSansKR-Regular.woff') format('woff');
+  `,
+
   // 각종 버튼
   button: `
     width: 100%;
     height: 35px;
     background-color: #1ed760;
     color: #fff;
+    cursor: pointer;
   `,
 
+  // 모달창
   centerModal: `
     border-radius: 10px;
     top: 50%;
--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,17 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 import spotifylogo from "../../assets/img/Spotify_Logo_RGB_Green.png";
 
 const FooterContainer = styled.footer`
+  position: absolute;
+  width: 100vw;
+  bottom: 0;
   padding: 20px 0;
-  background-color: ${(props) => props.theme.gray}; /** 나중에 삭제 */
   text-align: center;
-  /* position: fixed;
-  bottom: 0; */
 
   img {
     width: 70px;
+    margin: 10px;
+    vertical-align: middle;
+  }
+
+  p {
+    font-size: 12px;
+    color: #ddd;
   }
 `;
 
@@ -20,6 +28,9 @@ const Footer = () => {
     <FooterContainer>
       <div className="inner">
         <img src={spotifylogo} alt="스포티파이 로고" />
+        <p>
+          <Link to="https://github.com/smilemet/spotiplus">Github</Link>
+        </p>
       </div>
     </FooterContainer>
   );
